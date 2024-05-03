07ccb5e8d3d622b412593c6ddaed4bcd7b0ec8c9
Teng Zhang <rahxephon89@163.com>
2024-05-03 16:53:57 -0700

    reduce gas limit

--- aptos-move/e2e-tests/src/executor.rs
+++ aptos-move/e2e-tests/src/executor.rs
@@ -1171,7 +1171,7 @@ impl FakeExecutor {
                             gas_feature_version,
                             gas_params_res.unwrap().clone().vm,
                             storage_gas_params.unwrap(),
-                            10000000000000,
+                            100000000,
                         ),
                     )),
                     &mut TraversalContext::new(&storage),
